ab2ea7d6a26e76512e6038ea3811b144c6a6bee6
1995hyz <1995hyz@gmail.com>
2021-01-31 22:02:37 -0500

    Add tests on grid validation.

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,10 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,9 +15,6 @@ import java.util.stream.IntStream;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GridTest {
-
-    @Mock
-    Grid grid = new Grid(2);
 
     @Test
     public void test_reduceCellPossibleValues_shouldSucceed() throws Throwable {
@@ -43,8 +43,137 @@ public class GridTest {
     }
 
     @Test
-    public void test_validateGrid_shouldSucceed() {
+    public void test_validateGrid_shouldSucceed() throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/test/resources/possibleCellValues.json");
+        Map<String, List<Integer>> possibleValues = mapper.readValue(file, Map.class);
+        int subDimension = 2;
+        Grid grid = new Grid(subDimension);
+        Grid gridMocked = Mockito.spy(grid);
+        Mockito.when(gridMocked.getGridRowPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridRowPossibleValues(rowIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getGridColumnPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int columnIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridColumnPossibleValues(columnIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getSubGridPossibleValues(Mockito.anyInt(), Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            int columnIndex = (int) invocationOnMock.getArguments()[1];
+            return getSubGridPossibleValues(rowIndex, columnIndex, subDimension, possibleValues);
+        });
 
+        Assert.assertTrue("Fail to validate grid", gridMocked.validateGrid());
+    }
+
+    @Test
+    public void test_validateSubGrid_shouldFail() throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/test/resources/possibleCellValues_subGrid_fail.json");
+        Map<String, List<Integer>> possibleValues = mapper.readValue(file, Map.class);
+        int subDimension = 2;
+        Grid grid = new Grid(subDimension);
+        Grid gridMocked = Mockito.spy(grid);
+        Mockito.when(gridMocked.getGridRowPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridRowPossibleValues(rowIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getGridColumnPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int columnIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridColumnPossibleValues(columnIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getSubGridPossibleValues(Mockito.anyInt(), Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            int columnIndex = (int) invocationOnMock.getArguments()[1];
+            return getSubGridPossibleValues(rowIndex, columnIndex, subDimension, possibleValues);
+        });
+
+        Assert.assertFalse("Fail to validate grid", gridMocked.validateGrid());
+    }
+
+    @Test
+    public void test_validate_sameRow_shouldFail() throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/test/resources/possibleCellValues_sameRow_fail.json");
+        Map<String, List<Integer>> possibleValues = mapper.readValue(file, Map.class);
+        int subDimension = 2;
+        Grid grid = new Grid(subDimension);
+        Grid gridMocked = Mockito.spy(grid);
+        Mockito.when(gridMocked.getGridRowPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridRowPossibleValues(rowIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getGridColumnPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int columnIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridColumnPossibleValues(columnIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getSubGridPossibleValues(Mockito.anyInt(), Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            int columnIndex = (int) invocationOnMock.getArguments()[1];
+            return getSubGridPossibleValues(rowIndex, columnIndex, subDimension, possibleValues);
+        });
+
+        Assert.assertFalse("Fail to validate grid", gridMocked.validateGrid());
+    }
+
+    @Test
+    public void test_validate_sameColumn_shouldFail() throws Throwable {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/test/resources/possibleCellValues_sameColumn_fail.json");
+        Map<String, List<Integer>> possibleValues = mapper.readValue(file, Map.class);
+        int subDimension = 2;
+        Grid grid = new Grid(subDimension);
+        Grid gridMocked = Mockito.spy(grid);
+        Mockito.when(gridMocked.getGridRowPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridRowPossibleValues(rowIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getGridColumnPossibleValues(Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int columnIndex = (int) invocationOnMock.getArguments()[0];
+            return getGridColumnPossibleValues(columnIndex, subDimension * subDimension, possibleValues);
+        });
+        Mockito.when(gridMocked.getSubGridPossibleValues(Mockito.anyInt(), Mockito.anyInt())).thenAnswer(invocationOnMock -> {
+            int rowIndex = (int) invocationOnMock.getArguments()[0];
+            int columnIndex = (int) invocationOnMock.getArguments()[1];
+            return getSubGridPossibleValues(rowIndex, columnIndex, subDimension, possibleValues);
+        });
+
+        Assert.assertFalse("Fail to validate grid", gridMocked.validateGrid());
+    }
+
+    private Map<String, List<Integer>> getGridRowPossibleValues(int rowIndex, int gridDimension,
+                                                                Map<String, List<Integer>> possibleValues) {
+        Map<String, List<Integer>> rowPossibleValues = new HashMap<>();
+        for (int i=0; i<gridDimension; i++) {
+            String key = Integer.valueOf(rowIndex).toString() + Integer.valueOf(i).toString();
+            rowPossibleValues.put(key, possibleValues.get(key));
+        }
+        return rowPossibleValues;
+    }
+
+    private Map<String, List<Integer>> getGridColumnPossibleValues(int colIndex, int gridDimension,
+                                                                  Map<String, List<Integer>> possibleValues) {
+        Map<String, List<Integer>> colPossibleValues = new HashMap<>();
+        for (int i=0; i<gridDimension; i++) {
+            String key = Integer.valueOf(i).toString() + Integer.valueOf(colIndex).toString();
+            colPossibleValues.put(key, possibleValues.get(key));
+        }
+        return colPossibleValues;
+    }
+
+    private Map<String, List<Integer>> getSubGridPossibleValues(int rowIndex, int colIndex, int subGridDimension,
+                                                               Map<String, List<Integer>> possibleValues) {
+        Map<String, List<Integer>> subGridPossibleValues = new HashMap<>();
+        int rowSubGridPosition = rowIndex * subGridDimension;
+        int colSubGridPosition = colIndex * subGridDimension;
+        for(int i=0; i<subGridDimension; i++) {
+            for(int j=0; j<subGridDimension; j++) {
+                String key = Integer.valueOf(rowSubGridPosition+i).toString() + Integer.valueOf(colSubGridPosition+j).toString();
+                subGridPossibleValues.put(key, possibleValues.get(key));
+            }
+        }
+        return subGridPossibleValues;
     }
 
 }
