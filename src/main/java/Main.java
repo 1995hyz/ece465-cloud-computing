66f4635234e33b8e8189fe78e50221821100a56d
jkurian49 <jkurian49@gmail.com>
2021-02-01 12:28:25 -0500

    elapsed time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,12 +13,22 @@ public class Main {
         BlockingQueue<Grid> fringe = new ArrayBlockingQueue(1024);
         fringe.put(grid);
         BlockingQueue<Grid> explored_grids = new ArrayBlockingQueue(1024);
-        int num_threads = 5;
+        int num_threads = 1;
         AtomicInteger threads_waiting = new AtomicInteger(0);
         AtomicBoolean complete = new AtomicBoolean((false));
+        long start = System.nanoTime();
+        List threads = new ArrayList();
         for (int i = 0; i < num_threads; i++){
-            (new Thread(new Solver(i+1, fringe, explored_grids,threads_waiting, num_threads, complete))).start();
+            Thread t = (new Thread(new Solver(i+1, fringe, explored_grids,threads_waiting, num_threads, complete)));
+            t.start();
+            threads.add(t);
         }
+        for (int i = 0; i < num_threads; i++){
+            ((Thread) threads.get(i)).join();
+        }
+        long end = System.nanoTime();
+        long elapsedTime = end-start;
+        System.out.println(String.format("Grid solved in %f milliseconds.", elapsedTime/1e6));
         return;
     }
 }
